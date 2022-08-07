cbcfafc43cff1cce94a08ff93b6551a8f8974926
Erastus Murungi <erastuswagwan@gmail.com>
2022-08-07 13:04:03 -0700

    adding two tests for Interval tree constructor

--- tests/test_interval_tree.py
+++ tests/test_interval_tree.py
@@ -1,9 +1,20 @@
 from interval_tree import IntervalTree
 from interval_tree import gen_intervals
+import numpy as np
+import pytest
 
 
 class TestIntervalTree:
-    def test_creating_offline_creation(self):
+    def test_constructor_passes_given_valid_intervals(self):
         intervals = gen_intervals(20)
         root = IntervalTree.from_ndarray(intervals)
         assert len(root) == 20
+
+    def test_constructor_raises_exception_when_given_none_value_in_constructor(self):
+        with pytest.raises(TypeError):
+            IntervalTree.from_ndarray(None)
+
+    def test_constructor_raises_exception_when_given_wrongly_shaped_intervals(self):
+        intervals = np.random.randint(0, 100, (2, 2, 1))
+        with pytest.raises(ValueError):
+            IntervalTree.from_ndarray(intervals)
